6db29c12e05c64875c2ae0d565123e8183c42166
Everton Silva <everton.p.silva@outlook.com>
2024-05-11 02:00:55 -0300

    refactor: update movie card and carousel item UI

--- javascript/ui.js
+++ javascript/ui.js
@@ -1,10 +1,9 @@
 function createCarouselItem(movie, index) {
   return `
     <div class="carousel-item ${index === 0 ? 'active' : ''}">
-      <img src="https://image.tmdb.org/t/p/w500${movie.poster_path}" class="d-block w-100" alt="${movie.title}">
-      <div class="carousel-caption d-none d-md-block">
-        <h5>${movie.title}</h5>
-        <p>${movie.overview}</p>
+    <div class="carousel-caption d-none d-md-block">
+        <h5 class="text-dark">${movie.title}</h5>
+        <p class="text-dark">${movie.overview}</p>
       </div>
     </div>
   `;
@@ -20,7 +19,7 @@ function createMovieCard(movie) {
           <p class="card-text mb-auto">${movie.overview}</p>
           <p class="mt-auto"><small style="white-space: nowrap; overflow: hidden; text-overflow: ellipsis;">Data de lançamento: ${movie.release_date}</small></p>
           <p class="mt-auto"><small style="white-space: nowrap; overflow: hidden; text-overflow: ellipsis;">Classificação: ${movie.vote_average} / 10 (${movie.vote_count} votos)</small></p>
-          <a class="btn btn-outline-success" href="../movie_detail/detail.html">Detalhes</a>
+          <a class="btn btn-outline-success" href="../movie_detail/detail.html?movie_id=${movie.id}">Detalhes</a>
           </div>
       </div>
     </div>
